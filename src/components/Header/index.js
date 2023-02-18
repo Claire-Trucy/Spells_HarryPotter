4e558b4cf4c45beda5c57099b545f7f36b09198d
Claire-Trucy <trucy.claire92@gmail.com>
2023-02-18 13:44:18 +0100

    header version2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,36 @@
 import './styles.scss';
 import logoHP from './logoHP.png';
+import { useState } from 'react';
 
 function Header() {
+    const [showLinks, setShowLinks] = useState(false);
+
+    const handleShowLinks = () => {
+        setShowLinks(!showLinks);
+    }
+
     return (
-        <div className="header">
-            <ul className="header_menu">
-                <li className="header_menu_link"><a href='#'>Accueil</a></li>
-                <li className="header_menu_link"><a href='#'>Sorts</a></li>
-                <img src={logoHP} alt="HP logo" />
-                <li className="header_menu_link"><a href='#'>Contact</a></li>
-                <li className="header_menu_link"><a href='#'>A propos</a></li>
+        <div className={`header ${showLinks ? "show-nav" : "hide-nav"}`}>
+            <div className="header_logo">
+                <img src={logoHP}></img>
+            </div>
+            <ul className="header_links">
+                <li className="header_item slideInDown-1">
+                    <a href='#' className="header_link">Accueil</a>
+                </li>
+                <li className="header_item slideInDown-2">
+                    <a href='#' className="header_link">Sorts</a>
+                </li>
+                <li className="header_item slideInDown-3">
+                    <a href='#' className="header_link">Contact</a>
+                </li>
+                <li className="header_item slideInDown-4">
+                    <a href='#' className="header_link">A propos</a>
+                </li>
             </ul> 
+            <button className="header_burger">
+                <span className="burger-bar" onClick={handleShowLinks} ></span>
+            </button>
         </div>
     )
 }
